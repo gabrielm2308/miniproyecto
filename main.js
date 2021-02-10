3bb2861d6e35b7f3bd3ff71225476a9ecc1af92c
Your NameGabriel <agbalza23@gmail.com>
2021-02-09 21:10:29 -0400

    Funciona form

--- main.js
+++ main.js
@@ -17,3 +17,34 @@ function slider(){
     setTimeout("slider()",time);
 }
 window.onload = slider;
+
+
+function enviarform(){
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("correo").value;
+    const msje = document.getElementById("mensaje").value;
+    if(nombre===null || nombre===""){
+        document.getElementById("nombre").focus();
+    }else if(email===null || email===""){
+        document.getElementById("correo").focus();
+    }else if(msje===null || msje===""){
+        document.getElementById("mensaje").focus();
+    }else{
+        console.log("Nombre: "+nombre+
+        " Correo: "+email+
+        " Mensaje: "+msje);
+        alert("Enviado exitosamente");
+    }
+     
+}
+const email = document.getElementById("correo").value;
+function validarcorreo(email){
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var esvalido= expReg.test(email)
+    if (esvalido===true){
+        alert("El correo es válido");
+    }
+    else{
+        alert("EL CORREO NO ES VALIDO");
+    }
+}
